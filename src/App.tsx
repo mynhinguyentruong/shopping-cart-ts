349614135b291dac6e6829f290436d594cda6c10
mynhinguyentruong <mynhinguyentruong@gmail.com>
2022-08-09 11:40:32 -0400

    Setup routes to pages: Home, About, Store

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,20 @@
+
+import { Routes, Route } from 'react-router-dom'
+
+import { Container } from 'react-bootstrap'
+
+import { About } from './pages/About'
+import { Home } from './pages/Home'
+import { Store } from './pages/Store'
+
 export default function App() {
   return (
-    <h1>Hi</h1>
+    <Container className='mb4'>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/store" element={<Store />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Container>
   )
 }
