a1ef00cde92b0029eda05d2fc3468b117209c8e2
mynhinguyentruong <mynhinguyentruong@gmail.com>
2022-08-09 12:53:04 -0400

    Setup and style Card

--- src/components/StoreItem.tsx
+++ src/components/StoreItem.tsx
@@ -1,3 +1,5 @@
+import { Card } from 'react-bootstrap'
+
 type StoreItemProps = {
   id: number,
   name: string,
@@ -7,6 +9,14 @@ type StoreItemProps = {
 
 export function StoreItem({ id, name, price, imgUrl}: StoreItemProps) {
   return (
-    <h1>{name}</h1>
+    <Card>
+      <Card.Img variant="top" src={imgUrl} height="200px" style={{ objectFit: "cover" }} />
+      <Card.Body className='d-flex flex-column'>
+        <Card.Title className='d-flex justify-content-between align-items-baseline mb-4'>
+          <span className='fs-2'>{name}</span>
+          <span className='ms-2 text-muted'>{price}</span>
+        </Card.Title>
+      </Card.Body>
+    </Card>
   )
 }
