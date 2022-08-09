3924812d9a4f870a137ffe9f32cc03cc3bae337b
mynhinguyentruong <mynhinguyentruong@gmail.com>
2022-08-09 11:11:53 -0400

    Import BrowserRouter from react-router-dom and wrap around App component

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,13 @@ import App from './App'
 
 import "bootstrap/dist/css/bootstrap.min.css"
 
+import { BrowserRouter as Router } from 'react-router-dom'
+
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>
 )
