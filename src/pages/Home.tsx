96295eeb8a147ec4f7683cadd13baad256992276
mynhinguyentruong <mynhinguyentruong@gmail.com>
2022-08-10 14:04:04 -0400

    Add Link to Home Page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,10 @@
+import { Link } from 'react-router-dom'
+
 export function Home() {
   return (
     <> 
     <h1>Home</h1>
-    <p>Navigate to Store to start your order!</p>
+    <p>Navigate to <Link to='/store'> Store</Link> to start your order!</p>
     </>
   )
  
